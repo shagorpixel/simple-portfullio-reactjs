88565a2e270b094a4368fd7093cb1548c961e2eb
shagorpixel <shagorpixel@gmail.com>
2025-03-28 16:24:49 +0600

    Add Services Section

---
+++ src/Pages/Home/Data/Service.js
@@ -0,0 +1,28 @@
+
+import serviceImage1 from '../../../assets/img/sIcon-3.png';
+import serviceImage2 from '../../../assets/img/sIcon-1.png';
+import serviceImage3 from '../../../assets/img/sIcon-4.svg';
+import serviceImage4 from '../../../assets/img/sIcon-2.png';
+
+export const services = [
+    {
+        name:'Design',
+        image:serviceImage1,
+        description:'I specialize in web development and design, creating visually appealing, user-friendly digital experiences.'
+    },
+    {
+        name:'Branding',
+        image:serviceImage2,
+        description:"I'm a branding expert, crafting unique visual identities that tell your story and resonate with your audience."
+    },
+    {
+        name:'UX Research',
+        image:serviceImage3,
+        description:"I specialize in user experience research, collaborating on web development, and ensuring user-friendly digital products."
+    },
+    {
+        name:'Usability Testing',
+        image:serviceImage4,
+        description:"I specialize in web development and design, creating visually appealing, user-friendly digital experiences."
+    }
+]
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import HeroSection from './HeroSection';
 import About from './About';
+import Service from './Service';
 
 const Home = () => {
     return (
         <div>
             <HeroSection></HeroSection>
             <About></About>
+            <Service></Service>
         </div>
     );
 };
